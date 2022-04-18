70348e5e1f531a28c299448b5791982d40c06e88
LucasSoares1123 <lucaslebre138@gmail.com>
2022-04-18 13:18:40 -0300

    Refatorando o codigo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -12,15 +13,28 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Invasão Alien")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Inicializa o loop principal do jogo"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                pygame.display.flip()  # Sempre mostra a tela mais recente
-                self.screen.fill(self.settings.bg_color)
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Metodo acionado quando se aperta uma tecla ou mouse"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1  # move a nave para direita
+
+    def _update_screen(self):
+        """Atualiza as imagens na tela e muda para a proxima tela"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()  # Sempre mostra a tela mais recente
 
 
 if __name__ == '__main__':  # Cria uma instancia e da um start no game
--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         # configurações da Tela
         self.screen_width = 1200
         self.screen_height = 700
-        self.bg_color = (233, 124, 143)
+        self.bg_color = (0, 191, 255)
