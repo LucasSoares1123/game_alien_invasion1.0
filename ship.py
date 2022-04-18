d0aa3e1c977a5c9a2bd0531548c20a672b04f4d8
Lucas Soares <lucaslebre138@gmail.com>
2022-04-18 14:23:42 -0300

    Ajustando a velocidade da Nave

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicializa a nave em uma posição específica"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Carrega a imagem da nave e cria um Rect
@@ -16,16 +17,22 @@ class Ship:
         # Começa com a nave na parte de baixo da tela
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Guarda um valor decimal para a posição horizontal da Nave
+        self.x = float(self.rect.x)
+
         # Flag de movimento
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Atualiza o movimento da nave com base na Flag de movimento"""
+        # Atualiza o valor de X da Nave, não do Rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        # Aqui sim atulaliza o Rect da Nave no self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Desenha a nave na atual localização"""
